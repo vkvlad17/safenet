acfeb7b4c1e3e5dae918d0a2f6835c7205d5285f
VKVlad <43513367+VKVlad@users.noreply.github.com>
2024-06-04 12:12:52 +0200

    app

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-//export const API_BASE_URL="https://socialvlad-seven.vercel.app";
-export const API_BASE_URL="http://localhost:8080";
+export const API_BASE_URL="https://safenet.onrender.com";
+//export const API_BASE_URL="http://localhost:8080";
 
 export const api=axios.create({baseURL: API_BASE_URL, 
     headers: {
